34386e68417d3f8aa29302e2e4395453e1048bc1
irayspace <irayspacii@gmail.com>
2023-12-06 20:44:57 +0800

    refactor: moved init_config to __main__

--- reverse_proxy/__main__.py
+++ reverse_proxy/__main__.py
@@ -1,10 +1,13 @@
 import uvicorn
+from reverse_proxy.app.config import init_config, get_config
+
 
 def main() -> None:
+    init_config()
     uvicorn.run(
         'reverse_proxy.app.boot:get_app',
         host='0.0.0.0',
-        port=8000,
+        port=get_config('port') or 8000,
         reload=True,
     )
 
--- reverse_proxy/app/boot.py
+++ reverse_proxy/app/boot.py
@@ -1,11 +1,9 @@
 import yaml
 from fastapi import FastAPI
 from reverse_proxy.app.router import api_router
-from reverse_proxy.app.config import init_config
 
 
 def get_app() -> FastAPI:
     app = FastAPI()
     app.include_router(router=api_router)
-    init_config()
     return app
